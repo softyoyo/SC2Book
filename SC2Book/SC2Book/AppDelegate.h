bc188079b15efc2ed83c49b6024f39a2c0a94908
softyoyo <ha2tang@163.com>
2012-11-08 09:59:31 +0800

    建立工程

---
+++ SC2Book/SC2Book/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SC2Book
+//
+//  Created by Wistone#1 on 12-11-8.
+//  Copyright (c) 2012年 chenwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SC2Book/SC2Book/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SC2Book
+//
+//  Created by Wistone#1 on 12-11-8.
+//  Copyright (c) 2012年 chenwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
